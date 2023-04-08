4e61b71cd983e188c81b4775f40c6fbc64dcca81
chanhyoYun <chloeee2@naver.com>
2023-04-09 00:15:07 +0900

    first commit

---
+++ spartainven/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from spartainven.users.models import User
+from spartainven.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
